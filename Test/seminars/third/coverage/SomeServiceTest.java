073a95a60e2ca8b3c7db6f240e063f318c499158
Alex <kino_mac@mail.ru>
2023-12-03 23:17:12 +0300

    Initial commit

---
+++ Test/seminars/third/coverage/SomeServiceTest.java
@@ -0,0 +1,87 @@
+package seminars.third.coverage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeServiceTest {
+    private SomeService someService;
+
+    @BeforeEach
+    void setUp() {
+        someService = new SomeService();
+    }
+
+    @Test
+    void multipleThreeNotFiveReturnsFizz() {
+
+        String check = someService.fizzBuzz(3);
+
+        assertThat(check).isEqualTo("fizz");
+    }
+
+    @Test
+    void multipleFiveNotThreeReturnsBuzz() {
+
+        String check = someService.fizzBuzz(5);
+
+        assertThat(check).isEqualTo("buzz");
+    }
+
+    @Test
+    void multipleThreeOrFiveReturnsFizzBuzz() {
+
+        String check = someService.fizzBuzz(15);
+
+        assertThat(check).isEqualTo("fizzBuzz");
+    }
+
+    @Test
+    void multipleNotThreeOrFiveReturnsNumber() {
+
+        String check = someService.fizzBuzz(8);
+
+        assertThat(check).isEqualTo("8");
+    }
+
+    @Test
+    void testArrayCellValue6() {
+
+        Boolean check = someService.firstLast6(new int[]{1, 2, 3, 4, 5, 6});
+
+        assertEquals(true, check);
+    }
+
+    @Test
+    void testArrayCellValueNot6() {
+
+        Boolean check = someService.firstLast6(new int[]{1, 2, 3, 4, 5, 1});
+
+        assertFalse(check);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2000, 50, 1000", "2000, 100, 0", "2000, 0, 2000"})
+    void correctPercentDiscount(double amount, int discount, double result) {
+
+        assertThat(someService.calculatingDiscount(amount, discount))
+                .isEqualTo(result); // обычная скидка
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2000, 101, Скидка должна быть в диапазоне от 0 до 100%",
+            "2000, -10, Скидка должна быть в диапазоне от 0 до 100%",
+            "-2000, 10, Сумма покупки не может быть отрицательной"})
+    void notCorrectPercentDiscount(double amount, int discount, String result) {
+        assertThatThrownBy(() ->
+                someService.calculatingDiscount(amount, discount))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage(result); // процент скидки больше 100%
+    }
+
+}
